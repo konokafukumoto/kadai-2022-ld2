4222c3c6d7d1c9e7770df9a8d13bfba2039aab45
konokafukumoto <lt1206if@gmail.com>
2022-11-23 04:37:20 +0000

    課題2を提出

---
+++ kadai-02/index.js
@@ -0,0 +1,13 @@
+const firstname ="momo"
+const familyname = "hirai"
+const fullname = firstname + familyname
+console.log(fullname)
+
+const num = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(num)
+
+const dog = false
+const cat = true
+console.log(dog || cat)
+console.log(dog && cat)
+console.log(!dog || !cat)
